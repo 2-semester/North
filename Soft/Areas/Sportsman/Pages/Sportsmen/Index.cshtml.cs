f2c0503491b0892a81903e561c40060542e64dc0
Madli Kaevats <mkaeva@ttu.ee>
2020-05-06 09:17:17 +0300

    Sportsman softis

---
+++ Soft/Areas/Sportsman/Pages/Sportsmen/Index.cshtml.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+using North.Domain.Organization;
+using North.Domain.Sportsman;
+using North.Pages.Organization;
+using North.Pages.Sportsman;
+
+namespace North.Soft.Areas.Sportsman.Pages.Sportsmen
+{
+    public class IndexModel : SportsmenPage
+    {
+        public IndexModel(ISportsmenRepository r) : base(r)
+        {
+        }
+
+        public async Task OnGetAsync(string sortOrder,
+            string currentFilter, string searchString, int? pageIndex,string fixedFilter,string fixedValue)
+        {
+            await getList(sortOrder, currentFilter, searchString, pageIndex,fixedFilter,fixedValue );
+        }
+    }
+}
