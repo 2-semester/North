e5c939ceb04f295891c4a5a4692a76f61caa047d
Madli Kaevats <mkaeva@ttu.ee>
2020-05-07 12:29:26 +0300

    Database loomine

--- Infra/NorthDbInitializer.cs
+++ Infra/NorthDbInitializer.cs
@@ -1,11 +1,52 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using North.Data.SportCategory;
+using North.Data.Event;
+using North.Data.Organization;
 
 namespace North.Infra
 {
     public static class NorthDbInitializer
     {
+        internal static List<SportCategoryData> sportCategories => new List<SportCategoryData>
+        {
+            jooksmine, ujumine, surfamine, jalgrattasõit, discgolf
+        };
+        internal static List<EventData> eventsList => new List<EventData>
+        {
+            heaTeoJooks, parnuOpen, ristnaSoit
+        };
+        internal static List<OrganizationData> organizations => new List<OrganizationData>
+        {
+            
+        };
+        private static void InitializeOrganizations(NorthDbContext db)
+        {
+            if (db.Organizations.Count() != 0) return;
+            db.Organizations.AddRange(organizations);
+            db.SaveChanges();
+        }
+        private static void InitializeSportCategories(NorthDbContext db)
+        {
+            if (db.SportCategories.Count() != 0) return;
+            db.SportCategories.AddRange(sportCategories);
+            db.SaveChanges();
+        }
+        private static void InitializeEvents(NorthDbContext db)
+        {
+            if (db.Events.Count() != 0) return;
+            db.Events.AddRange(eventsList);
+            db.SaveChanges();
+        }
+
+        public static void Initialize(NorthDbContext db)
+        {
+            InitializeSportCategories(db);
+            InitializeEvents(db);
+            InitializeOrganizations(db);
+        }
+
         internal static SportCategoryData jooksmine = new SportCategoryData
         {
             Id = "Jooksmine", Name = "Jooksmine", 
@@ -32,23 +73,47 @@ namespace North.Infra
         {
             Id = "Jalgrattasõit", Name = "Jalgrattasõit",
         };
-        
-        internal static List<SportCategoryData> sportCategories => new List<SportCategoryData>
+
+
+        internal static EventData heaTeoJooks = new EventData
         {
-            jooksmine, ujumine, surfamine, jalgrattasõit, discgolf
+            Id = "Heateo Jooks",
+            Name = "Heateo Jooks",
+            EventDate = Convert.ToDateTime("07/05/20"),
+            SportCategoryId="Jooksmine",
+            TypeId="rahvajooks"
         };
 
-        private static void InitializeSportCategories(NorthDbContext db)
+        internal static EventData parnuOpen = new EventData
         {
-            if (db.SportCategories.Count() != 0) return;
-            db.SportCategories.AddRange(sportCategories);
-            db.SaveChanges();
-        }
+            Id = "ITB Pärnu Open 2020",
+            Name = "ITB Pärnu Open 2020",
+            EventDate = Convert.ToDateTime("12/08/20"),
+            SportCategoryId = "Discgolf",
+            TypeId = "karikasari"
+        };
+        internal static EventData ristnaSoit = new EventData
+        {
+            Id = "Ristna öösõit",
+            Name = "Ristna öösõit",
+            EventDate = Convert.ToDateTime("06/06/21"),
+            SportCategoryId = "Jalgrattasõit",
+            TypeId = "maastikusõit"
+        };
 
-        public static void Initialize(NorthDbContext db) 
+        internal static OrganizationData SEB = new OrganizationData
         {
-            InitializeSportCategories(db);
-        }
+            Id = "SEB",
+            Name = "SEB",
+            Definition = "Eesti innovatiivseim pank"
+        };
+        internal static OrganizationData sportland = new OrganizationData
+        {
+            Id = "Sportland",
+            Name = "Sportland",
+            Definition = "Muuta noorte inimeste elu huvitavamaks, paremaks ja emotsionaalsemaks läbi pakutavate toodete ning teenuste."
+        };
+
     }
 }
 
