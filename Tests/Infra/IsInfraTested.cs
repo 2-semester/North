a4e109426b4a7644b2c7c9cc6cae49b26defebb9
Madli Kaevats <mkaeva@ttu.ee>
2020-05-11 12:00:31 +0300

    tests fix1

--- Tests/Infra/IsInfraTested.cs
+++ Tests/Infra/IsInfraTested.cs
@@ -7,13 +7,13 @@ namespace North.Tests.Infra
     {
         private const string assembly = "North.Infra";
 
-        protected override string Namespace(string name) { return $"{assembly}.Areas.{name}"; }
+        protected override string Namespace(string name) { return $"{assembly}.{name}"; }
 
         [TestMethod] public void IsEventTested() { isAllTested(assembly, Namespace("Event")); }
         [TestMethod] public void IsEventListTested() { isAllTested(assembly, Namespace("EventList")); }
         [TestMethod] public void IsOrganizationTested() { isAllTested(assembly, Namespace("Organization")); }
         [TestMethod] public void IsSportsmanTested() { isAllTested(assembly, Namespace("Sportsman")); }
         [TestMethod] public void IsSportsmanEventTested() { isAllTested(assembly, Namespace("SportsmanEvent")); }
-        [TestMethod] public void IsTested() { isAllTested(base.Namespace("Soft")); }
+        [TestMethod] public void IsTested() { isAllTested(base.Namespace("Infra")); }
     }
 }
